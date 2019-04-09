a8b7a2e438fd7eaa21e72eea321fc4697a16c723
mayukhnandy <mayukhnandy13@gmail.com>
2019-04-09 14:21:41 +0530

    99

--- JavaDemoApp/src/com/model/User.java
+++ JavaDemoApp/src/com/model/User.java
@@ -20,7 +20,7 @@ public class User {
 	}
 	public void setPassword(String password) {
 		this.password = password;
-		//Comment Added for Backlog 90 modifiedd.
+		//Comment Added for Backlog 91 modifiedd.
 		//Comment added for Backlog 39
 	}
 }
