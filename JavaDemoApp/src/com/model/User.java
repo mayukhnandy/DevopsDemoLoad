74263514de0c5dd7a21a983098d53d6840f01957
sk203 <sk203@demo1.sr.kovair.com>
2019-04-11 09:14:53 -0700

    118

--- JavaDemoApp/src/com/model/User.java
+++ JavaDemoApp/src/com/model/User.java
@@ -20,7 +20,7 @@ public class User {
 	}
 	public void setPassword(String password) {
 		this.password = password;
-		//Comment added for backlog 113
-		// comment for 114
+		//Comment 1
+		// Comment added for Backlog 118
 	}
 }
