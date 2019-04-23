811d6e60238905a6b7cf820071d7ae5c3e60ff8b
mayukhnandy <mayukhnandy13@gmail.com>
2019-04-23 05:34:01 -0700

    6

--- JavaDemoApp/src/com/model/User.java
+++ JavaDemoApp/src/com/model/User.java
@@ -15,7 +15,7 @@ public class User {
 	public void setUserId(String userId) {
 		this.userId = userId;         
 	}
-	public String getPassword() {
+	public String getPassword() {69
 		return password;
 	}
 	public void setPassword(String password) {
